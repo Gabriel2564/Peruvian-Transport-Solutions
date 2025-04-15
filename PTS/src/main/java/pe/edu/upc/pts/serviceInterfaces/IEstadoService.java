27efed51cb30fc1e39bad6fd5c3c48c40f622c01
Kiara Hualla Martínez <u20211d621@upc.edu.pe>
2025-04-15 14:52:13 -0500

    create IEstadoService

---
+++ PTS/src/main/java/pe/edu/upc/pts/serviceInterfaces/IEstadoService.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.peruviantransportsolutions.servicesInterfaces;
+
+import pe.edu.upc.peruviantransportsolutions.entities.Estado;
+
+import java.util.List;
+
+public interface IEstadoService {
+    public List<Estado> listar_estado();
+
+    List<Estado> list();
+}
