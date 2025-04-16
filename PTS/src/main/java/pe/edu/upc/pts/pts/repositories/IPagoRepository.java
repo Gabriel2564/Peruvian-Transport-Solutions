0f2a87aa96b5f9c10c5e97df7d45f5b78f5379c4
u20221b798 <u20221b798@upc.edu.pe>
2025-04-15 21:34:16 -0500

    Delete PTS/src/main/java/pe/edu/upc/pts/pts directory

--- PTS/src/main/java/pe/edu/upc/pts/pts/repositories/IPagoRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.pts.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.pts.entities.Pago;
-
-public interface IPagoRepository extends JpaRepository<Pago, Integer> {
-}
