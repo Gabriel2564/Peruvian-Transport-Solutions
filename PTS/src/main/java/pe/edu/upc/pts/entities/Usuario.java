8ecbec5ac9a8bcdeacc4e9902a2e86f5d7ca9574
rominasososo <u202111487@upc.edu.pe>
2025-04-20 09:45:01 -0500

    Merge branch 'main' of https://github.com/Gabriel2564/Peruvian-Transport-Solutions into Romina

---
+++ PTS/src/main/java/pe/edu/upc/pts/entities/Usuario.java
@@ -0,0 +1,46 @@
+package pe.edu.upc.pts.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_usuario;
+    @Column(name = "username",nullable = false,length = 100)
+    private String username;
+    @Column(name = "password",nullable = false,length = 100)
+    private String password;
+    public Usuario() {}
+
+    public Usuario(int id_usuario, String username, String password) {
+        this.id_usuario = id_usuario;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
