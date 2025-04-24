0666d634970138cef8fb42cc0c6e67c7e42432a2
u20221b798 <u20221b798@upc.edu.pe>
2025-04-23 20:49:40 -0500

    Update IPagoService.java

--- PTS/src/main/java/pe/edu/upc/pts/serviceInterfaces/IPagoService.java
+++ PTS/src/main/java/pe/edu/upc/pts/serviceInterfaces/IPagoService.java
@@ -1,11 +1,11 @@
 package pe.edu.upc.pts.serviceInterfaces;
+
 import pe.edu.upc.pts.entities.Pago;
 import java.util.List;
 
 public interface IPagoService {
     List<Pago> list();
-    void insert(Pago pago);
-    void update(Pago pago);
+    void insert(Pago payment);
+    void update(Pago payment);
     void delete(int id);
 }
-
