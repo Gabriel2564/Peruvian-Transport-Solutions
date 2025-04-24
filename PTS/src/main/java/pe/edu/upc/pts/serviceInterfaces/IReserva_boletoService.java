367f85c17094fd8ba4a28e16ee98c61aeb22c0c4
u20221b798 <u20221b798@upc.edu.pe>
2025-04-23 20:49:57 -0500

    Update IReserva_boletoService.java

--- PTS/src/main/java/pe/edu/upc/pts/serviceInterfaces/IReserva_boletoService.java
+++ PTS/src/main/java/pe/edu/upc/pts/serviceInterfaces/IReserva_boletoService.java
@@ -4,8 +4,6 @@ import pe.edu.upc.pts.dtos.Reserva_boletoDTO;
 import java.util.List;
 
 public interface IReserva_boletoService {
-    List<Reserva_boletoDTO> listarReservas();
-    Reserva_boletoDTO insertarReserva(Reserva_boletoDTO dto);
+    List<Reserva_boletoDTO> listReservations();
+    Reserva_boletoDTO insertReservation(Reserva_boletoDTO dto);
 }
-
-
