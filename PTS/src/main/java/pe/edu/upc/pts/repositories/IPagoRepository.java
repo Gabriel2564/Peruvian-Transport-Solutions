9a3fef5916779ca55a6f51ca9412f892222219b8
u20221b798 <u20221b798@upc.edu.pe>
2025-04-25 11:46:37 -0500

    Update IPagoRepository.java

--- PTS/src/main/java/pe/edu/upc/pts/repositories/IPagoRepository.java
+++ PTS/src/main/java/pe/edu/upc/pts/repositories/IPagoRepository.java
@@ -1,7 +1,20 @@
 package pe.edu.upc.pts.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.pts.entities.Pago;
 
+import java.util.List;
+
+
 public interface IPagoRepository extends JpaRepository<Pago, Integer> {
+    @Query("SELECT p FROM Pago p WHERE p.paymentType = :paymentType")
+    List<Pago> findByPaymentType(@Param("paymentType") String paymentType);
+
+    @Query("SELECT p.paymentType, COUNT(p) FROM Pago p GROUP BY p.paymentType")
+    List<Object[]> countPaymentsByType();
+
 }
