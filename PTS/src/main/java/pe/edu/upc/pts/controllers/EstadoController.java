8a7160a00e240452f3d7590eb8ea667dd76f3681
Kiara Hualla Martínez <u20211d621@upc.edu.pe>
2025-04-29 15:18:15 -0500

    ESTADO CONTROLLER CON LOGS

--- PTS/src/main/java/pe/edu/upc/pts/controllers/EstadoController.java
+++ PTS/src/main/java/pe/edu/upc/pts/controllers/EstadoController.java
@@ -1,5 +1,6 @@
 package pe.edu.upc.pts.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +14,14 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/estados")
+@Slf4j
 public class EstadoController {
     @Autowired
     private IEstadoService eS;
 
     @GetMapping
     public List<EstadoDTO> listar() {
-
+        log.info("Solicitud GET para listar estados");
         return eS.listar_estado().stream().map(x->{
             ModelMapper m=new ModelMapper();
             return m.map(x,EstadoDTO.class);
@@ -27,6 +29,7 @@ public class EstadoController {
     }
     @PostMapping
     public void insertar(@RequestBody EstadoDTO dto){
+        log.info("Solicitud POST para insertar estado: {}", dto.getState_type());
         dto.setIdEstado(0); //Omite cualquier valor que este en el id, se genera automaticamente segun la secuencia
         ModelMapper m = new ModelMapper();
         Estado e = m.map(dto,Estado.class);
@@ -34,6 +37,7 @@ public class EstadoController {
     }
     @PutMapping
     public void modificar(@RequestBody EstadoDTO dto){
+        log.info("Solicitud PUT para modificar estado con ID: {}", dto.getIdEstado());
         ModelMapper m = new ModelMapper();
         Estado e = m.map(dto,Estado.class);
         eS.update(e);
@@ -41,6 +45,7 @@ public class EstadoController {
 
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable("id") Integer id){
+        log.warn("Solicitud DELETE para eliminar estado con ID: {}", id);
         eS.delete(id);
     }
 
