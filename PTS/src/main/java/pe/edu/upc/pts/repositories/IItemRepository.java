a6b22cb74800a0ef64c2b780fe7a0240109aa314
u20221b798 <u20221b798@upc.edu.pe>
2025-05-13 12:18:28 -0500

    Import ItemRepository

--- PTS/src/main/java/pe/edu/upc/pts/repositories/IItemRepository.java
+++ PTS/src/main/java/pe/edu/upc/pts/repositories/IItemRepository.java
@@ -3,6 +3,7 @@ package pe.edu.upc.pts.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.pts.entities.Item;
 
 import java.util.List;
